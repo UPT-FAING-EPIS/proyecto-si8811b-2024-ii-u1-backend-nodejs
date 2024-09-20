7204ee665ce8d07ef4315eb6d9063cb8b075a37d
jesus20202 <jesdelgado05@gmail.com>
2024-09-19 21:11:51 -0500

    add-swagger-logs #6

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -5,6 +5,33 @@ import { handleInputErrors } from '../middleware/validation'
 
 const router = Router()
 
+/**
+ * @swagger
+ * /api/v1/auth/create-account:
+ *   post:
+ *     summary: Crear una cuenta
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               password_confirmation:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Cuenta creada exitosamente
+ *       400:
+ *         description: Error de validación
+ */
+
 router.post('/create-account',
     body('name')
         .notEmpty().withMessage('No puede ir vacio'), 
@@ -22,12 +49,56 @@ router.post('/create-account',
     AuthController.createAccount
 )
 
+/**
+ * @swagger
+ * /api/v1/auth/confirm-account:
+ *   post:
+ *     summary: Confirmar cuenta
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               token:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Cuenta confirmada exitosamente
+ *       400:
+ *         description: Error de validación
+ */
+
 router.post('/confirm-account',
     body('token')
        .notEmpty().withMessage('Token no puede ir vacio'),
     handleInputErrors,
     AuthController.confirmAccount
 )
+
+/**
+ * @swagger
+ * /api/v1/auth/login:
+ *   post:
+ *     summary: Iniciar sesión
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Inicio de sesión exitoso
+ *       400:
+ *         description: Error de validación
+ */
 
 router.post('/login', 
     body('email')
@@ -38,12 +109,54 @@ router.post('/login',
     AuthController.login
 )
 
+/**
+ * @swagger
+ * /api/v1/auth/request-code:
+ *   post:
+ *     summary: Solicitar código de confirmación
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Código de confirmación enviado
+ *       400:
+ *         description: Error de validación
+ */
+
 router.post('/request-code', 
     body('email')
        .isEmail().withMessage('E-mail no valido'),
     handleInputErrors,
     AuthController.requestConfirmationCode
 )
+
+/**
+ * @swagger
+ * /api/v1/auth/forgot-password:
+ *   post:
+ *     summary: Solicitar restablecimiento de contraseña
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Instrucciones para restablecimiento de contraseña enviadas
+ *       400:
+ *         description: Error de validación
+ */
 
 router.post('/forgot-password',  
     body('email')
@@ -52,12 +165,62 @@ router.post('/forgot-password',
     AuthController.forgotPassword
 )
 
+/**
+ * @swagger
+ * /api/v1/auth/validate-token:
+ *   post:
+ *     summary: Validar token de restablecimiento de contraseña
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               token:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Token validado exitosamente
+ *       400:
+ *         description: Error de validación
+ */
+
 router.post('/validate-token',
     body('token')
        .notEmpty().withMessage('Token no puede ir vacio'),
     handleInputErrors,
     AuthController.validateToken
 )
+
+/**
+ * @swagger
+ * /api/v1/auth/update-password/{token}:
+ *   post:
+ *     summary: Actualizar contraseña con token
+ *     parameters:
+ *       - name: token
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               password:
+ *                 type: string
+ *               password_confirmation:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Contraseña actualizada exitosamente
+ *       400:
+ *         description: Error de validación
+ */
 
 router.post('/update-password/:token',
     param('token')
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,31 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import { connectDB } from './config/db'
-import authRoutes from './routes/authRoutes'
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db';
+import authRoutes from './routes/authRoutes';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import options from '../swaggerOptions'; // importa el archivo de opciones de Swagger
+import logger from '../src/logs/logger'; // importa el archivo de configuración del logger
 
-dotenv.config()
-connectDB()
+dotenv.config();
+connectDB();
 
 const app = express();
-// app.use(cors(corsConfig))
 
-app.use(express.json())
+// Configura Swagger
+const swaggerDocs = swaggerJsDoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.use('/api/v1/auth', authRoutes)
+// Middleware para loguear las solicitudes
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url}`);
+  next();
+});
 
-export default app
+app.use(express.json());
+app.use('/api/v1/auth', authRoutes);
+
+// Log cuando el servidor esté corriendo
+
+
+export default app;
